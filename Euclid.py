558a85e4742d01ef60b2f508213176df04393434
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2022-04-10 11:34:47 -0400

    gcd with steps is working

--- Euclid.py
+++ Euclid.py
@@ -10,3 +10,63 @@ def euclid_gcd(a, b):
         r = a % b
     gcd = b
     return gcd
+
+
+def euclid_gcd_with_steps(a, b):
+    r_list = [a, b]
+    r = a % b
+    while r:
+        a = b
+        b = r
+        r = a % b
+        r_list.append(r)
+    return r_list
+
+
+def euclid_extended_algorithm(a, b):
+    if a == 0:
+        return b, 0, 1
+    gcd, temp_y, temp_x = euclid_extended_algorithm(b % a, a)
+    x = temp_x - (b // a) * temp_y
+    y = temp_y
+    return gcd, x, y
+
+
+def menu():
+    print('------- Menu --------')
+    print('gs: gcd with intermediate steps shown')
+    print('gcd: gcd')
+    print('ee: Euclid Extended Algorithm')
+    print('rsa: Simplified RSA Keys')
+    print('e: exit')
+
+
+def gcd_steps_io():
+    print('')
+    first = int(input('First number: '))
+    second = int(input('Second number: '))
+    steps_list = euclid_gcd_with_steps(first, second)
+    for number in steps_list:
+        print(number, end=' ')
+    print()
+    input('*** any key to continue ***')
+
+
+def select_task():
+    choice = input('What would you like to do?\n')
+    if choice == 'gs':
+        gcd_steps_io()
+    if choice == 'e':
+        return 'e'
+
+
+def main():
+    while True:
+        menu()
+        task = select_task()
+        if task == 'e':
+            break
+
+
+if __name__ == '__main__':
+    main()
--- test_Euclid.py
+++ test_Euclid.py
@@ -1,4 +1,4 @@
-from Euclid import euclid_gcd
+from Euclid import euclid_gcd, euclid_gcd_with_steps, euclid_extended_algorithm
 
 
 def test_euclid_gcd():
@@ -6,3 +6,17 @@ def test_euclid_gcd():
     prime2 = 23
     gcd_result = euclid_gcd(prime1, prime2)
     assert gcd_result == 1
+
+
+def test_euclid_gcd_with_steps():
+    prime1 = 17
+    prime2 = 23
+    gcd_result = euclid_gcd_with_steps(prime1, prime2)
+    assert gcd_result == [6, 5, 1, 0]
+
+
+def test_euclid_extended_algorithm():
+    num1 = 10
+    num2 = 25
+    extended_algorithm_result = euclid_extended_algorithm(num1, num2)
+    assert extended_algorithm_result == (5, -2, 1)
