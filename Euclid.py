c4a0b8eb78dc6d530c459f076f2efa44e41a03af
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2022-04-10 12:21:13 -0400

    gcd and Euclid's extended algorithm working

--- Euclid.py
+++ Euclid.py
@@ -24,6 +24,7 @@ def euclid_gcd_with_steps(a, b):
 
 
 def euclid_extended_algorithm(a, b):
+    return_list = []
     if a == 0:
         return b, 0, 1
     gcd, temp_y, temp_x = euclid_extended_algorithm(b % a, a)
@@ -34,8 +35,8 @@ def euclid_extended_algorithm(a, b):
 
 def menu():
     print('------- Menu --------')
-    print('gs: gcd with intermediate steps shown')
     print('gcd: gcd')
+    print('gs: gcd with intermediate steps shown')
     print('ee: Euclid Extended Algorithm')
     print('rsa: Simplified RSA Keys')
     print('e: exit')
@@ -49,13 +50,35 @@ def gcd_steps_io():
     for number in steps_list:
         print(number, end=' ')
     print()
-    input('*** any key to continue ***')
+    input('*** press enter to continue ***')
+
+
+def gcd_io():
+    print('')
+    first = int(input('First number: '))
+    second = int(input('Second number: '))
+    gcd = euclid_gcd(first, second)
+    print(f'gcd: {gcd}')
+    input('*** press enter to continue ***')
+
+
+def euclid_extended_algorithm_io():
+    print('')
+    first = int(input('First number: '))
+    second = int(input('Second number: '))
+    ee = euclid_extended_algorithm(first, second)
+    print(f'gcd: {ee}')
+    input('*** press enter to continue ***')
 
 
 def select_task():
     choice = input('What would you like to do?\n')
+    if choice == 'gcd':
+        gcd_io()
     if choice == 'gs':
         gcd_steps_io()
+    if choice == 'ee':
+        euclid_extended_algorithm_io()
     if choice == 'e':
         return 'e'
 
